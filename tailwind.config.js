d5e58f313f1d65b1b337a0bc994a480fa844a20a
RiddellLink <riddelllink@gmail.com>
2024-01-12 15:54:30 -0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.html',
+    // './src/**/*.{js,jsx}', // Uncomment if you use Tailwind in JS/JSX files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
